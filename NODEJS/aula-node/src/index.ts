687f3a08b366e75290af5b7fe34c99fbf283493a
pertusatti22 <pertusatti22@gmail.com>
2022-02-11 17:42:57 -0300

    Estrutura do CRUD de usuários

--- NODEJS/aula-node/src/index.ts
+++ NODEJS/aula-node/src/index.ts
@@ -1,2 +1,19 @@
+import express,{ Request, Response, NextFunction } from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
-console.log("execute!");
+const app = express();
+
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de Rotas
+app.use(usersRoute);
+app.use(statusRoute);
+
+// Inicialização do servidor
+app.listen(3000, () => {
+    console.log('Aplicação online na porta 3000!');
+});
+
